9417040c11920477e58374700711f4de5aefc688
Tom Johnson <tom@dp.la>
2016-02-04 16:01:50 -0800

    Add base container
    Implementing a base container at '/'.

--- app/server.rb
+++ app/server.rb
@@ -12,6 +12,49 @@ module Derby
 
     configure { set :repository, RDF::Repository.new }
 
+    get '/' do
+      begin 
+        RDF::LDP::Resource.find(RDF::URI(request.url), settings.repository)
+      rescue RDF::LDP::NotFound 
+        RDF::LDP::Container.new(RDF::URI(request.url), settings.repository)
+          .create('', 'text/turtle')
+      end
+    end
+
+    get '/*' do
+      RDF::LDP::Resource.find(RDF::URI(request.url), settings.repository)
+    end
+
+    patch '/*' do
+      RDF::LDP::Resource.find(RDF::URI(request.url), settings.repository)
+    end
+
+    post '/*' do
+      RDF::LDP::Resource.find(RDF::URI(request.url), settings.repository)
+    end
+
+    put '/*' do
+      begin
+        RDF::LDP::Resource.find(RDF::URI(request.url), settings.repository)
+      rescue RDF::LDP::NotFound
+        model = request.env.fetch('HTTP_LINK', '')
+        RDF::LDP::Resource.interaction_model(model)
+          .new(RDF::URI(request.url), settings.repository)
+      end
+    end
+
+    options '/*' do
+      RDF::LDP::Resource.find(RDF::URI(request.url), settings.repository)
+    end
+
+    head '/*' do
+      RDF::LDP::Resource.find(RDF::URI(request.url), settings.repository)
+    end
+
+    delete '/*' do
+      RDF::LDP::Resource.find(RDF::URI(request.url), settings.repository)
+    end
+
     run! if app_file == $0
   end
 end
---
+++ spec/app/server_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+require 'rack/test'
+
+require 'server'
+
+describe Derby::Server do
+  include Rack::Test::Methods
+
+  let(:app) { described_class }
+  
+  describe '/' do
+    describe 'GET' do
+      it 'is ay basic container' do
+        get '/'
+
+        expect(last_response['Link'])
+          .to include RDF::LDP::CONTAINER_CLASSES[:basic].to_s
+      end
+    end
+  end
+end
